45ba4d5e922b8635da9f819acc7459b3b64b82ff
Le Nguyen <lenguyen25061996@gmail.com>
2017-11-17 14:26:03 +0700

    Learn UDKJS

--- ioasync.js
+++ ioasync.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var path = process.argv[2];
-var content = fs.readFileSync(path);
+var content = fs.readFile(path);
 var count = (content.toString().match(/\n/g) || []).length;
 console.log(count);
